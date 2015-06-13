91a5509e5c0ef0e695be75761c54f3a2bcc6e567
Rob <robianmcd@gmail.com>
2015-06-13 16:17:24 -0400

    let heroku set the port

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.post('/', function(req, res) {
 
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
     console.log('Listening');
 });
